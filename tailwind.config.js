fbb0076d27ec9e326894ca302a51b150874d911a
PolCarva <pablocarvalhogimenez@gmail.com>
2023-11-30 17:56:17 -0300

    Add new colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#009ad9",
+          hover: "#0081b3",
+          dark: {
+            DEFAULT: "#006699",
+            hover: "#005580",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
